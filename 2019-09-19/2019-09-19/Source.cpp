34f2c3595a3c4f6ffafd3636cc1cfb483ced62f1
Richiemilliemilty <55443426+Richiemilliemilty@users.noreply.github.com>
2019-09-19 08:42:46 -0400

    Add files via upload

---
+++ 2019-09-19/2019-09-19/Source.cpp
@@ -0,0 +1,12 @@
+// MIlton Allen 
+// Intro to Computer Science 
+// period 1 
+// Mr.willams 
+// 2019-09-19 Homework
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Name: Milton Allen" << std::endl;
+}
