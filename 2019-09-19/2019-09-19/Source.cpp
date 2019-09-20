641ffcdc9db7c27ce92b4016f424b1f9394d746d
Richiemilliemilty <55443426+Richiemilliemilty@users.noreply.github.com>
2019-09-20 08:35:41 -0400

    Update Source.cpp

--- 2019-09-19/2019-09-19/Source.cpp
+++ 2019-09-19/2019-09-19/Source.cpp
@@ -9,4 +9,14 @@
 int main()
 {
 	std::cout << "Name: Milton Allen" << std::endl;
+	std::cout << "Period 1: Computer Science" << std::endl
+	std::cout << "Period 2: GLobal" << std::endl
+	std::cout << "Period 3: Health" << std::endl
+	std::cout << "Period 4: Earth science << std::endl
+	std::cout << "Period 5: English" << std::endl
+	Std::cout << "Period 6: Spanish" << std::endl
+	Std::cout << "Period 7: Gym" << std::endl
+	std::cout << "Period 8: Lunch" << std::endl
+	std::cout << "Period 9: Geometry" << std::endl
+	std::cout << "Period 10: House" << std::endl;
 }
